963b79e64617d218a441443e3e129d09f59bb03d
Brice Arnaud <brice.arnaud@inria.fr>
2022-05-31 10:23:43 +0200

    Modif control with keyboard : no longer need to use spacebar to stop the wheelchair

--- bin/Joystick/joystick_key.py
+++ bin/Joystick/joystick_key.py
@@ -1,8 +1,20 @@
+"""
+
+This Python script is used to control the wheelchair using the keyboard arrows.
+You must have launched the whole project except the Joystick part.
+Then run this script and use the arrows to move and 'q' to quit.
+WARNNING : When you release a key, the chair takes around 0.5s to stop.
+"""
+
+
+
 import threading
 import time
 import paho.mqtt.client as mqtt
 from magick_joystick.Topics import *
 from click import getchar
+
+PERIOD = 1/30
 
 ARR_U = '\x1b[A'
 ARR_D = '\x1b[B'
@@ -13,12 +25,17 @@ ARR_L = '\x1b[D'
 
 x = 0
 y = 0
+running = True
+timeCpt = 0
 
 def get_kbd_arrows():
     global x
     global y
-    while True:
+    global running
+    global timeCpt
+    while running:
         key = getchar()
+        timeCpt = 15
         if (key == ARR_U):
             #print("Move UP")
             y = 127
@@ -31,9 +48,24 @@ def get_kbd_arrows():
         elif (key == ARR_R):
             #print("Move RIGHT")
             x = 127
-        elif( key == ' '):
+        elif (key == 'q'):
+            running = False
+    return
+
+#After some time without any key, reset x and y to 0
+def stopContinuousMove():
+    global x
+    global y
+    global running
+    global timeCpt
+    while running:
+        if timeCpt > 0:
+            timeCpt -= 1
+        else:
+            #print("Reseted")
             x = 0
             y = 0
+        time.sleep(PERIOD)
 
 
 client = mqtt.Client()
@@ -41,25 +73,28 @@ client.connect("localhost", 1883, 60)
 client.loop_start()
 
 print("Mqtt connection opened")
-time.sleep(1)
+time.sleep(0.5)
 
 #On envoit le fait que l'on souhaite piloter
 msg = action_drive(True)
 client.publish(msg.TOPIC_NAME, msg.serialize())
 
-time.sleep(1)
+time.sleep(0.5)
 print("Action drive sended, starting loop...")
 
-d = threading.Thread(target=get_kbd_arrows, daemon=True)
-d.start()
+daemon = threading.Thread(target=get_kbd_arrows, daemon=True)
+daemon.start()
+daemon2 = threading.Thread(target=stopContinuousMove, daemon=True)
+daemon2.start()
 
-while True:
-    button = 0
-    long_click = 0
 
-    #print("(X=%d,Y=%d)" % (x,y))
+button = 0
+long_click = 0
+while running:
+    #print("(X=%d,Y=%d) %d" % (x,y, timeCpt))
 
     joy_data = joystick_state(button, x, y, long_click)
     client.publish(joy_data.TOPIC_NAME, joy_data.serialize())
-    time.sleep(1 / 30)
+    time.sleep(PERIOD)
 
+joy_data = joystick_state(button, 0, 0, long_click)
